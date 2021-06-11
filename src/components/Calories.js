0068ab3ed190bfe26a9c1bb159bc3486ee5684a5
alaeddine-zeraib <alaeddine.zeraib@gmail.com>
2021-06-11 01:01:17 +0100

    renew version

--- src/components/Calories.js
+++ src/components/Calories.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-class Calories extends Component {
-
-	render(){
-		return (
-			<div className="sous-header">
+const Calories = (props) => {
+	return (
+		<div className="sous-header">
         		<h2 style={{paddingTop: '25px'}}>
 					You have 
-					<span className="badge rounded-pill bg-success"><b>{this.props.calorie} calories</b></span> 
+					<span className="badge rounded-pill bg-success"><b>{props.calorie} calories</b></span> 
 					 to consume Today in Food!
 				</h2>
 				<br/>
 			</div>
-		);
-	}
-
-} 
+	)
+}
 
 function mapStateToProps(state) {
 	return {
--- src/components/Food.js
+++ src/components/Food.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { addFoodById, removeCalorieById } from '../actions';
 
-class Food extends Component {
-	render() {
-		return (
-			<div className="col-md-4 food-bg">
-				<h2 className="text-center">Food Items</h2>
-				<ul className="list-group">
-					{this.props.food.map((item) => {
-						return <li key={item.id} 
-										className="list-group-item"
-										onClick={()=>{
-											this.props.addFoodById(item.id);
-											this.props.removeCalorieById(item.id);
-											}
+const Food = (props) => {
+	return (
+		<div className="col-md-4 food-bg">
+			<h2 className="text-center">Food Items</h2>
+			<ul className="list-group">
+				{props.food.map((item) => {
+					return <li key={item.id} 
+									className="list-group-item"
+									onClick={()=>{
+										props.addFoodById(item.id);
+										props.removeCalorieById(item.id);
 										}
-										>
-							<span className="badge bg-info text-dark"><b>{item.name}</b></span> - 
-							<span className="badge bg-primary"> {item.weight} g</span> - 
-							<span className="badge bg-warning text-dark"> {item.calories} cal</span>
-							
-						</li>
-					})}
-				</ul>
-			</div>
-		)
-	}
+									}
+									>
+						<span className="badge bg-info text-dark"><b>{item.name}</b></span> - 
+						<span className="badge bg-primary"> {item.weight} g</span> - 
+						<span className="badge bg-warning text-dark"> {item.calories} cal</span>
+						
+					</li>
+				})}
+			</ul>
+		</div>
+	)
 }
 
 function mapStateToProps(state) {
